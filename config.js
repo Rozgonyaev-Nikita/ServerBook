5f1891a8f2af42fba7e2b1bbf5b53dcaa03429eb
Nikita Rozgonyaev <karp.karp24karp@gmail.com>
2025-02-20 15:22:41 +0300

    Задеплоил бд

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 export const PORT = process.env.PORT || 5000;
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book';
+// export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book';
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://dbKarp:MZFgZ3cPMP2uxHsM@atlascluster.swjds7w.mongodb.net/Books?retryWrites=true&w=majority';
+;
