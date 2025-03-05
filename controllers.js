53e0f9dd9e5db0985b071c5fb4bd3e3d8c3e47c8
Daniil <62511931+KARPorativ@users.noreply.github.com>
2025-03-05 21:08:24 +0300

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -146,53 +146,6 @@ export const register = async (req, res) => {
 // }
 // };
 
-export const exchangeBook = async (req, res) => {
-  try {
-    const {
-      author_surname,
-      author_name,
-      book_title,
-      isbn,
-      year,
-      genre,
-      condition,
-      userId,         // Assuming userId is passed in the request body
-    } = req.body;
 
-    // Create and save the author
-    let author = await Author.findOne({ lastName: author_surname, firstName: author_name });
-    if (!author) {
-      author = new Author({ lastName: author_surname, firstName: author_name });
-      await author.save();
-    }
-
-    let bookLibrary = await BookLibrary.findOne({ idAuthor: author._id, bookName: book_title });
-    if (!bookLibrary) {
-      bookLibrary = new BookLibrary({ idAuthor: author._id, bookName: book_title });
-      await bookLibrary.save();
-    }
-
-    // Create and save the category (you might want to check if the category already exists)
-    // const category = new CategorySchema({ category: genre }); // Make sure the field name is correct
-    // await category.save();
-
-    // Create and save the offer list entry
-    const offerList = new OfferList({
-      idBookLibrary: bookLibrary._id,
-      idUser: userId,                // Use the userId from the request body
-      IBSN: isbn,
-      yearPublishing: new Date(year), // Assuming year is a number like 2023
-      category: genre,     // If you want to link the category to the offer
-      status: condition
-    });
-    await offerList.save();
-
-    // Respond with success message
-    res.status(201).json({ message: 'Все правильно', offerList });
-  } catch (error) {
-    // Handle errors and respond appropriately
-    // res.status(500).json({ message: 'ошибка', error });
-    res.status(500).json({ message: 'Error creating offer', karp: req.body, error });
-  }
 >>>>>>> 928619264b8b7aa8b4914d860f594e6e6c087360
 };
