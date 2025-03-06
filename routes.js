e3facaa7b174340ad05cc17be8ad3c48f9606275
Nikita Rozgonyaev <karp.karp24karp@gmail.com>
2025-03-06 09:07:53 +0400

    исправил

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { login, register, changeUser } from './controllers.js';
+import express from "express";
+import { login, register, exchangeBook, changeUser } from "./controllers.js";
 
 const router = express.Router();
 
-router.get('/login', login);
-router.post('/registration', register);
-// router.post('/exchangeBook', exchangeBook);
-router.put('/changeUser', changeUser);
+router.get("/login", login);
+router.post("/registration", register);
+router.post("/exchangeBook", exchangeBook);
+router.put("/changeUser", changeUser);
 export default router;
